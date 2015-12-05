811242a3e374aa4e69bfbea5818ade303f72baa0
Alex McLean <alexmclean206@berkeley.edu>
2015-12-05 13:25:56 -0800

    added basic parsing for SQL table creation

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var util = require('./utility');
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
+
 
 app.post('/update', function (req, res, next) {
   util.parseTable(req, res, next);
+});
+
+app.post('/parse', function (req, res, next) {
+  util.parseORMSequelize(req, res, next);
 });
 
 app.listen(port);
