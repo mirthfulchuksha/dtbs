010efddd97dc97da6047ab496c5871fbf36498c9
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-12-04 16:40:11 -0800

    D3 button included

--- client/controllers/d3layout.js
+++ client/controllers/d3layout.js
@@ -30,36 +30,47 @@ angular.module('DTBS.directives', [])
   .directive('d3Bars', ['d3Service', function (d3Service) {
     return {
       restrict: 'EA',
-      scope: {
-        data: '=info'
-      },
+      scope: {},
       link: function(scope, element, attrs) {
-        d3Service.d3().then(function(d3) {
+        d3Service.d3().then(function (d3) {
           // d3 code goes here
           var svg = d3.select(element[0])
           .append("svg")
           .style('width', '100%');
-          scope.$watch('data', function(newVals, oldVals) {
-            console.log("hey")
-            return scope.render(newVals);
-          }, true);
 
-          scope.render = function (data) {
-            // remove all previous items before render
-            svg.selectAll('*').remove();
-            // If we don't pass any data, return out of the element
-            if (!data) return;
-            svg.selectAll('rect')
-            .data(data).enter()
-            .append('rect')
-            .attr('height', 50)
-            .attr('width', 50)
-            .style('background-color', red)
-          };
-          // set up watch to see if button clicked; add div
         });
       }};
-  }]);
+  }])
+.directive('d3Schema', ['d3Service', function (d3Service) {
+  return {
+    restrict: 'EA',
+    scope: {},
+    link: function (scope, element, attrs) {
+      d3Service.d3().then(function (d3) {
+        scope.render = function (data) {
+          // remove all previous items before render
+          var svg = d3.selectAll('svg');
+          svg.selectAll('*').remove();
+          // If we don't pass any data, return out of the element
+          if (!data) return;
+          svg.selectAll('rect')
+          .data(data).enter()
+          .append('rect')
+          .attr('height', 50)
+          .attr('width', 50)
+          .style('background-color', red)
+        };
+
+        // set up watch to see if button clicked; add rectangle with render func
+        scope.$watch('data', function(newVals, oldVals) {
+          console.log("hey")
+          return scope.render(newVals);
+        }, true);
+
+      })
+    }
+  }
+}]);
 
 
 
