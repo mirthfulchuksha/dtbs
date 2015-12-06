1add13572cdbb85d983dba5364bf3f6d0743af5f
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-12-05 18:12:21 -0800

    Creates parsing function for the Bookshelf mySQL ORM

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ app.post('/parse', function (req, res, next) {
   util.parseORMSequelize(req, res, next);
 });
 
+app.post('/bookshelf', function (req, res, next) {
+  util.parseORMBookshelf(req, res, next);
+});
+
 app.listen(port);
 
 console.log('Server listening on port: ' + port);
