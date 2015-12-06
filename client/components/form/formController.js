21ddb36c1192914ed13697730e5203095028b58f
Alex McLean <alexmclean206@berkeley.edu>
2015-12-05 17:23:08 -0800

    added ace js editor and hooked up backend sql translation

--- client/components/form/formController.js
+++ client/components/form/formController.js
@@ -1,11 +1,11 @@
 angular.module('DTBS.main')
-.controller('FormController', ['$scope', '$timeout', function ($scope, $timeout) {
+.controller('FormController', ['$scope', '$timeout', 'CodeParser', function ($scope, $timeout, CodeParser) {
     //object for table storage
     $scope.tableStorage = {};
     //incrementing id for table creation in child scopes
     $scope.id = 0;
 
-    var secondsToWaitBeforeSave = 3;
+    var secondsToWaitBeforeSave = 0;
 
     $scope.addTable = function (table) {
       $scope.tableStorage[table.id] = table;
@@ -20,13 +20,13 @@ angular.module('DTBS.main')
 
     $scope.removeKeyFromTable = function (index, table) {
       $scope.tableStorage[table.id].attrs.splice(index,1);
-      console.log("deleted!");
     };
 
     var timeout = null;
     var saveUpdates = function() {
      if ($scope.tableStorage) {
        console.log("Saving updates to item #" + Object.keys($scope.tableStorage).length + "...", $scope.tableStorage);
+       CodeParser.fetchCode($scope.tableStorage);
      } else {
        console.log("Tried to save updates to item #" + ($scope.tableStorage.length) + " but the form is invalid.");
      }
@@ -47,7 +47,6 @@ angular.module('DTBS.main')
 
   }])
   .controller('TableController', ['$scope', 'd3Data', function ($scope, d3Data) {
-    var secondsToWaitBeforeSave = 3;
     $scope.table = {};
     //Table save function that clears form and pushes up to the parent
     $scope.save = function (name) {
@@ -62,3 +61,30 @@ angular.module('DTBS.main')
     };
 
   }])
+  .factory('CodeParser', function ($http) {
+    
+    var fetchCode = function (tables) {
+      var dataObj = {data: []};
+      for(var table in tables) {
+        dataObj.data.push(tables[table]);
+      }
+      console.log(dataObj);
+
+      return $http({
+        method: 'POST', 
+        url: '/update',
+        data : dataObj
+      }).then(function (res) {
+        //places data on editor
+        var editor = ace.edit("editor");
+        editor.setValue(res.data);
+        //still highlights the text on addition, why u no maintain ur library :(
+        editor.$blockScrolling = Infinity;
+        return res.data;
+      });
+    };
+
+    return {
+      fetchCode: fetchCode
+    };
+  });
