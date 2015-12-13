91d404fecea17cadbb14e9c22ed9733f8d6ef5f4
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-12-12 17:22:27 -0800

    Add password hashing

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -4,7 +4,37 @@ var autoIncrement = require('mongoose-auto-increment');
 
 var userModel = mongoose.Schema({
   _id: Number,
-  userName: {type: String, required: true}
+  userName: {type: String, required: true},
+  password: {
+    type: String,
+    required: true
+  },
+  salt: String
+});
+
+userModel.methods.comparePasswords = function (password) {
+  var savedPassword = this.password;
+  // Load hash from your password DB.
+  bcrypt.compare(password, savedPassword, function(err, res) {
+    if (err) { console.error(err) }
+    return res;
+  });
+};
+
+userModel.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  bcrypt.genSalt(function (err, salt) {
+    if (err) { console.error(err) }
+
+    bcrypt.hash(this.password, salt, null, function (err, hash) {
+      if (err) { console.error(err) }
+      this.password = hash;
+      this.salt = salt;
+    });
+  });
 });
 
 userModel.plugin(autoIncrement.plugin, 'User');
