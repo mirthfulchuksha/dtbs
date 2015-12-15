ed77645bbd23a7115f853c4ddec0d0316f2c1eda
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-12-15 15:08:52 -0800

    Start front-end refactor with modularity and materialize styling

--- client/app.routes.js
+++ client/app.routes.js
@@ -6,6 +6,34 @@ angular.module('DTBS', [
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/', {
+      templateUrl: './components/getStarted/splash/splash.html'
+    })
+    .when('/start', {
+      templateUrl: './components/getStarted/start/start.html'
+    })
+    .when('/login', {
+      templateUrl: './components/getStarted/login/login.html',
+      controller: 'ModalCtrl'
+    })
+    .when('/signup', {
+      templateUrl: './components/getStarted/signup/signup.html'
+    })
+    .when('/try', {
+      templateUrl: './components/getStarted/setup/setup.html'
+    })
+    .when('/new', {
+      templateUrl: './components/getStarted/setup/setup.html'
+    })
+    .when('/saved', {
+      templateUrl: './components/getStarted/saved/saved.html'
+    })
+    .when('/mongo', {
+      templateUrl: './components/mongo/mongo.html'
+    })
+    .when('/sql', {
+      templateUrl: './components/sql/sql.html'
+    })
+    .when('/table', {
       templateUrl: './components/form/formView.html'
     });
 });
--- client/shared/modal/modal.module.js
+++ client/shared/modal/modal.module.js
@@ -51,7 +51,8 @@ mymodal.controller('ModalCtrl', ['$scope', 'CodeParser', 'SaveAndRedirectFactory
       data: $scope.user
     }).success(function (data, status, headers, config) {
       console.log("Logged in!");
-      $scope.toggleLoginModal();
+      // $scope.toggleLoginModal();
+      $('#loginModal').closeModal();
     }).error(function (data, status, headers, config) {
       console.log("Cannot log in");
     });
@@ -105,44 +106,44 @@ mymodal.factory('SaveAndRedirectFactory', ['AccessSchemaService', '$http', funct
   };
 }]);
 
-mymodal.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' +
-          '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-              '<div class="modal-header">' +
-          '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-          '<h4 class="modal-title">{{ title }}</h4>' +
-              '</div>' +
-              '<div class="modal-body" ng-transclude></div>' +
-            '</div>' +
-          '</div>' +
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink (scope, element, attrs) {
-        scope.title = attrs.title;
+// mymodal.directive('modal', function () {
+//     return {
+//       template: '<div class="modal fade">' +
+//           '<div class="modal-dialog">' +
+//             '<div class="modal-content">' +
+//               '<div class="modal-header">' +
+//           '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+//           '<h4 class="modal-title">{{ title }}</h4>' +
+//               '</div>' +
+//               '<div class="modal-body" ng-transclude></div>' +
+//             '</div>' +
+//           '</div>' +
+//         '</div>',
+//       restrict: 'E',
+//       transclude: true,
+//       replace:true,
+//       scope:true,
+//       link: function postLink (scope, element, attrs) {
+//         scope.title = attrs.title;
 
-        scope.$watch(attrs.visible, function (value) {
-          if (value === true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
+//         scope.$watch(attrs.visible, function (value) {
+//           if (value === true)
+//             $(element).modal('show');
+//           else
+//             $(element).modal('hide');
+//         });
 
-        $(element).on('shown.bs.modal', function () {
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
+//         $(element).on('shown.bs.modal', function () {
+//           scope.$apply(function(){
+//             scope.$parent[attrs.visible] = true;
+//           });
+//         });
 
-        $(element).on('hidden.bs.modal', function () {
-          scope.$apply(function () {
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
+//         $(element).on('hidden.bs.modal', function () {
+//           scope.$apply(function () {
+//             scope.$parent[attrs.visible] = false;
+//           });
+//         });
+//       }
+//     };
+//   });
