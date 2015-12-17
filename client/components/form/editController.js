07fcc67dcdfb493fdd017e374256b90e8ac9d49a
Joy Johnson <iwasjoy@gmail.com>
2015-12-16 17:36:08 -0800

    Implemented ability to delete fields in tables and also, if deleted field is primary key, deletion of any foreign keys linked to the field.

--- client/components/form/editController.js
+++ client/components/form/editController.js
@@ -3,12 +3,15 @@ angular.module('DTBS.main')
 
   $scope.keyEdit = [];
 
+  $scope.tablename;
+
   $scope.editKeysModal = false;
   $scope.toggleEditKeysModal = function () {
     $scope.editKeysModal = !$scope.editKeysModal;
   };
 
   $scope.editTable = function(table){
+    $scope.tablename = table;
     for (var key in $scope.tableStorage){
       if ($scope.tableStorage[key]["name"] === table) {
         for (var key2 in $scope.tableStorage[key]["attrs"]){
@@ -25,9 +28,30 @@ angular.module('DTBS.main')
     $scope.toggleEditKeysModal();
   };
 
-}])
+  $scope.deleteField = function (fieldId) {
 
-// .controller('EditKeysController', ['$scope', function ($scope) {
+    var foreign = $scope.tablename + "_" + fieldId;
+    
+    //delete requested field and foreign keys linking to the field, if any
+    for (var key2 in $scope.tableStorage){
+      for (var key3 in $scope.tableStorage[key2]){
+        for (var i = 0; i < $scope.tableStorage[key2][key3].length; i++){
+          if ($scope.tableStorage[key2][key3][i].id === fieldId || $scope.tableStorage[key2][key3][i].id === foreign){
+            $scope.tableStorage[key2][key3].splice(i, 1);
+          }
+        }
+      }
+    }
+    
+    //to remove items from editKeyModal as they are removed:
+    for (var i = 0; i < $scope.keyEdit.length; i++){
+      if ($scope.keyEdit[i].id === fieldId){
+        $scope.keyEdit.splice(i, 1);
+      }
+    }
 
-  
-// }])
+    $scope.interactd3();
+
+  };
+
+}]);
