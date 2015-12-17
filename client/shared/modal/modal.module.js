d117a6a675fcd06df926127fdc123846c1786fea
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-12-17 12:33:48 -0800

    Revert

--- client/shared/modal/modal.module.js
+++ client/shared/modal/modal.module.js
@@ -1,21 +1,9 @@
 var mymodal = angular.module('DTBS.modal', []);
 
-mymodal.controller('ModalCtrl', ['$scope', 'CodeParser', 'SaveAndRedirectFactory', '$http', '$location', function ($scope, CodeParser, SaveAndRedirectFactory, $http, $location) {
-  $scope.showLoginModal = true;
-  $scope.loggingIn = true;
-  $scope.db = {lang: "SQL"};
 
-  $scope.$watch(function() { return $location.path(); }, function(newValue, oldValue){
-    switch (newValue) {
-      case '/login':
-        $('#loginModal').openModal();
-        break;
-      case '/setup':
-        $('#setupModal').openModal();
-        break;
-      default:
-    }
-  });
+mymodal.controller('ModalCtrl', ['$scope', 'CodeParser', 'SaveAndRedirectFactory', '$http', function ($scope, CodeParser, SaveAndRedirectFactory, $http) {
+  $scope.showModal = false;
+  $scope.showLoginModal = true;
 
   $scope.toggleModal = function (){
     $scope.showModal = !$scope.showModal;
@@ -55,66 +43,19 @@ mymodal.controller('ModalCtrl', ['$scope', 'CodeParser', 'SaveAndRedirectFactory
     canvas.remove();
   };
 
-  $scope.sendUserData = function (options, cb1, cb2) {
-    $http(options).success(cb1).error(cb2);
-  };
-
   $scope.user = {};
   $scope.login = function () {
-      $scope.user.login = true;
-      CodeParser.update(null, null, $scope.user);
-      $scope.sendUserData({
-        url: '/login',
-        method: 'POST',
-        data: $scope.user
-      }, function (res) {
-        $('#loginModal').closeModal();
-        $scope.notValid = false;
-        $location.path('/setup');
-      }, function (res) {
-        if (res === 'noUser') {
-          $scope.noUser = true;
-          $scope.notValid = false;
-        } else {
-          $scope.notValid = true;
-        }
-      });
-      $scope.user = {};
-  };
-
-  $scope.signup = function () {
-    CodeParser.update(null, null, $scope.user);
-    if ($scope.isMatch()) {
-      $scope.sendUserData({
-        url: '/signup',
-        method: 'POST',
-        data: $scope.user
-      }, function () {
-        $('#loginModal').closeModal();
-        $scope.notValid = false;
-        $scope.userExist = false;
-        $location.path('/setup');
-      }, function () {
-        $scope.userExist = true;
-      });
-      $scope.user = {};
-    }
-  };
-
-  $scope.fetchDBs = function () {
-    CodeParser.fetchSchemas();
-  };
-
-  $scope.isMatch = function () {
-    if ($scope.user.password !== $scope.pass2) {
-      $scope.notMatch = true;
-      $scope.noUser = false;
-      $scope.notValid = false;
-      return false;
-    } else {
-      $scope.notMatch = false;
-      return true;
-    }
+    $http({
+      url: '/login',
+      method: 'POST',
+      data: $scope.user
+    }).success(function (data, status, headers, config) {
+      console.log("Logged in!");
+      $scope.toggleLoginModal();
+    }).error(function (data, status, headers, config) {
+      console.log("Cannot log in");
+    });
+    $scope.user = {};
   };
 
   $scope.githubRedirect = function () {
@@ -122,9 +63,10 @@ mymodal.controller('ModalCtrl', ['$scope', 'CodeParser', 'SaveAndRedirectFactory
     SaveAndRedirectFactory.stashTables();
   };
 
+  $scope.db = {lang: "mySQL"};
   $scope.updateFactory = function () {
     switch ($scope.db.lang) {
-      case "SQL":
+      case "mySQL":
         $scope.db.fileName = $scope.db.lang + '_Schema.sql';
         break;
       case "Bookshelf":
